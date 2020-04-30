e8ce6337c19542f58ced7eecc1027087a7fed77d
root <root@ip-10-0-1-55.us-east-2.compute.internal>
2020-04-30 00:18:37 +0000

    first commit

---
+++ common-install.sh
@@ -0,0 +1,92 @@
+#!/bin/bash
+
+# get release version
+RELEASE=$(cat /etc/redhat-release)
+YUM_ARGS="--setopt=tsflags=nodocs"
+
+
+# shared packages
+# - build tools for building gems       +# add files
+# - iproute needed for ip command to get ip addresses   +ADD run.sh fluentd.conf.template passwd.template fluentd-check.sh ${HOME}/
+# - nss_wrapper used to support username identity       +ADD common-*.sh /tmp/
+# - bc for calculations in run.conf
+PACKAGES="gem gcc-c++ libcurl-devel make bc gettext nss_wrapper hostname iproute git git-core zlib zlib-devel patch readline readline-devel libyaml-devel libffi-devel openssl-devel  bzip2 autoconf automake libtool bison curl sqlite-devel which"
+
+# ruby packages
+#PACKAGES="${PACKAGES} rh-ruby22 rh-ruby22-rubygems rh-ruby22-ruby-devel"
+
+# if the release is a red hat version then we need to set additional arguments for yum repositories
+RED_HAT_MATCH='^Red Hat.*$'
+if [[ $RELEASE =~ $RED_HAT_MATCH && -z "$USE_SYSTEM_REPOS" ]]; then
+  #NOTE: Until the first yum command is run, /etc/yum.repos.d/redhat.repo contains no repositories, so yum-config-manager will not enable/disable anything.
+  #This command will force the population of said file, see #https://access.redhat.com/solutions/1443553
+  yum repolist --disablerepo=* && yum-config-manager --disable \* > /dev/null
+  #Set YUM_ARGS
+  YUM_ARGS="${YUM_ARGS} --enablerepo=rhel-7-server-rpms --enablerepo=rhel-server-rhscl-7-rpms --enablerepo=rhel-7-server-optional-rpms"
+  #subscription-manager repos --enable $YUM_ARGS
+  #yum-config-manager --enable $YUM_ARGS
+fi
+
+# enable epel when on CentOS
+CENTOS_MATCH='^CentOS.*'
+if [[ $RELEASE =~ $CENTOS_MATCH && -z "$USE_SYSTEM_REPOS" ]]; then
+  rpmkeys --import file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7
+  cd /tmp
+  yum install -y epel-release centos-release-scl-rh
+  curl -sSL https://rvm.io/mpapis.asc | gpg --import -
+  curl -sSL https://rvm.io/mpapis.asc | gpg2 --import -
+  curl -sSL https://rvm.io/pkuczynski.asc | gpg2 --import -
+  curl -L get.rvm.io |bash -s stable
+  source /etc/profile.d/rvm.sh
+  rvm reload
+  rvm requirements run
+  rvm install 2.5.2
+fi
+
+# ensure latest versions
+yum update $YUM_ARGS -y
+
+# install all required packages
+yum install -y $YUM_ARGS $PACKAGES
+
+# clean up yum to make sure image isn't larger because of installations/updates
+yum clean all
+rm -rf /var/cache/yum/*
+rm -rf /var/lib/yum/*
+
+# set home directory
+mkdir -p ${HOME} && \
+
+# install gems for target version of fluentd, eventually
+# update to fluentd version that matches version deployed
+# into openshift
+gem install -N --conservative --minimal-deps --no-document \
+  fluentd:${FLUENTD_VERSION} \
+  'activesupport:<5' \
+  'public_suffix:<3.0.0' \
+  'fluent-plugin-record-modifier:<1.0.0' \
+  'fluent-plugin-rewrite-tag-filter:<2.0.0' \
+  fluent-plugin-kubernetes_metadata_filter \
+  fluent-plugin-rewrite-tag-filter \
+  fluent-plugin-secure-forward \
+  'fluent-plugin-remote_syslog:<1.0.0' \
+  fluent-plugin-splunk-ex \
+  fluent-plugin-splunkhec
+
+# set up directores so that group 0 can have access like specified in
+# https://docs.openshift.com/container-platform/3.7/creating_images/guidelines.html
+# https://docs.openshift.com/container-platform/3.7/creating_images/guidelines.html#openshift-specific-guidelines
+mkdir -p /etc/fluent
+chgrp -R 0 /etc/fluent
+chmod -R g+rwX /etc/fluent
+chgrp -R 0 ${HOME}
+chmod -R g+rwX ${HOME}
+chgrp -R 0 /etc/pki
+chmod -R g+rwX /etc/pki
+mkdir /secrets
+chgrp -R 0 /secrets
+chmod -R g+rwX /secrets
+chgrp -R 0 /var/log
+chmod -R g+rwX /var/log
+
+
